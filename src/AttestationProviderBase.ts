effacef1dc093e2e04f267ff511b4ab038671763
Yogeshwar Chaudhari <chaudhari.yogesh20@gmail.com>
2022-07-06 00:24:02 +0930

    added a common _attestationToken property to AttestationProviderBase

--- src/AttestationProviderBase.ts
+++ src/AttestationProviderBase.ts
@@ -1,3 +1,12 @@
 export abstract class AttestationProviderBase {
+  protected _attestationToken!: string;
   abstract getDeviceIntegrity(): boolean;
+
+  get attestationToken() {
+    return this._attestationToken;
+  }
+
+  set attestationToken(token: string) {
+    this._attestationToken = token;
+  }
 }
